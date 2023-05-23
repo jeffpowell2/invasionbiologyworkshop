c147193d8ef016699a6850f8eb62cc363d973f91
Jeff Powell <jeffpowell2@gmail.com>
2023-05-23 11:03:54 +1000

    pushing the mess that is Tuesday morning

--- scripts/classCode_turnover.R
+++ scripts/classCode_turnover.R
@@ -28,3 +28,56 @@ otu %>%
   t() -> mat
 
 
+library(vegan)
+temp <- rarecurve(mat, step=1000, tidy=TRUE)
+left_join(meta, temp %>% rename(SampleID=Site)) %>% 
+  ggplot(aes(x=Sample, y=Species, colour=site_code, group=SampleID)) + 
+  geom_line() + 
+  facet_wrap(~site_code)
+hist(log10(rowSums(mat)))
+sort(rowSums(mat))[1:25]
+temp<-rrarefy(mat, 5000)
+matr <- temp[rowSums(temp)==5000, ]
+dim(matr)
+
+ecm_otus <- filter(tax, guild=='ectomycorrhizal')$OTU_ID
+left_join(meta, mat %>% 
+            as.data.frame() %>% 
+            dplyr::select(ecm_otus) %>% 
+            rownames_to_column('SampleID')) -> dat_ecm
+
+library(indicspecies)
+multipatt(dat_ecm %>% select(starts_with('OTU_')), 
+          dat_ecm$site_code) -> res
+summary(res)
+
+mat_ecm <- dat_ecm %>% select(starts_with('OTU_'))
+adonis2(mat_ecm ~ site_code, data=dat_ecm, add=TRUE)
+
+rda1 <- rda(mat_ecm ~ site_code, data=dat_ecm)
+rda1
+plot(rda1)
+anova(rda1)
+
+mat_ecm1 <- decostand(mat_ecm, method='pa')
+cap1 <- capscale(mat_ecm1 ~ site_code, data=dat_ecm, distance='bray', add=TRUE)
+cap1
+anova(cap1)
+plot(cap1)
+ordiplot(cap1)
+summary(cap1)
+
+scrs_spp <- scores(cap1, display='species')
+scrs_site <- scores(cap1, display='sites') # TIDY
+head(scrs_site)
+
+cbind(dat_ecm, scrs_site) %>% 
+  ggplot(aes(x=CAP1, y=CAP2, colour=site_code)) + 
+  geom_point() -> p
+plotly::ggplotly(p)
+
+head(scores(cap1, display='lc'))
+
+head(scrs_spp[order(scrs_spp[, 'CAP2'], decreasing=TRUE), ]) -> inv_otus
+tax %>% 
+  filter(OTU_ID %in% rownames(inv_otus))
